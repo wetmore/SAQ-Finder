5cf48c134da0fb68f0663e8d0c09a1e5c6a48ba1
wetmore <wetmore.matt@gmail.com>
2012-04-13 17:58:59 -0400

    Search for SAQs based on lat/lng, not postal code
    Instead of looking up stores based on postal code, the PHP script now
    searches based on latitude/longitude. This allows results for every
    query, fixing the bug where no stores were returned because Google
    was returning only the postal code prefix. This change also allows
    returning more than just 5 stores in the future, but that is not
    implemented in this commit.

--- app/scripts/geo.php
+++ app/scripts/geo.php
@@ -6,27 +6,14 @@ $data = file_get_contents($url);
 $decdata = json_decode($data);
 $arr = get_object_vars($decdata);
 $status = $arr["status"];
-$postal = "";
 $days = array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
 $pos = array();
 if ($status == "OK") {
   $arr = $arr["results"];
   $arr = get_object_vars($arr[0]);
   $geometry = $arr["geometry"];
-  $arr = $arr["address_components"];
-  $foundPostal = False;
-  foreach ($arr as $value) {
-    $component = get_object_vars($value);
-    if ($component["types"][0] == "postal_code") {
-      $postal = preg_replace('/\s+/','',$component["short_name"]);
-      $foundPostal = True;
-    }
-  }
   $pos[0] = $geometry->location->lat;
   $pos[1] = $geometry->location->lng;
-  if (!($foundPostal)) {
-    echo "none";
-  }
 } elseif ($status == "ZERO_RESULTS") {
   echo "none";
 } elseif ($status == "INVALID_REQUEST") {
@@ -34,16 +21,16 @@ if ($status == "OK") {
 } else {
   echo "other";
 }
-if ($postal != "") {
-  $base_url = "http://www.saq.com/webapp/wcs/stores/servlet/afficherCarte?pwidth=494&pheight=324&maxSearchResults=5&pageResults=20&coderegion=index.html&storeId=10001&catalogId=10001&langId=-1&country=CA&stateProvince=QC&transaction=search&searchQuantifier=AND&radius=2000&maxSearchResults=5&postalCode=";
-  $url = $base_url . $postal;
+if (sizeof($pos) != 0) {
+  $url = "http://www.saq.com/webapp/wcs/stores/servlet/RechercheSuccursale?langId=-1&storeId=10001&catalogId=10001&country=CA&latitude=" . $pos[0] . "&longitude=" . $pos[1] . "&transaction=search&searchQuantifier=AND&radius=10&maxSearchResults=5&units=km";
   $stores = array();
   $stores[0] = $pos;
   $page = file_get_contents($url);
   $regex = "/this.(user[1-9]|address|latitude|longitude) = '(.*)'/";
   preg_match_all($regex,$page,$matches);
   $current_index = 0;
-  for($i=4;$i < count($matches[1]);$i++) {
+  //var_dump($matches);
+  for($i=8;$i < count($matches[1]);$i++) {
     switch ($matches[1][$i]) {
       case "address":
         $current_index++;
@@ -62,7 +49,7 @@ if ($postal != "") {
         $stores[$current_index]['type'] = $matches[2][$i];
       break;
       case "user8":
-        preg_match_all("/([0-9]{2}:[0-9]{2})/",$matches[2][$i],$times_uf);
+        preg_match_all("/(F|[0-9]{2}:[0-9]{2})/",$matches[2][$i],$times_uf);
         $times = array();
         for ($j = 0; $j < 7; $j++) {
           $times[$j] = array(
